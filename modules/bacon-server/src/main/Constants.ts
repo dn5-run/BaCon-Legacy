264efe18237fccd52dd3db86b9d9b8226d07d333
ddPn08 <pyn.g.1125@gmail.com>
2022-01-29 16:19:38 +0900

    fix | correct the name of the data directory

--- modules/bacon-server/src/main/Constants.ts
+++ modules/bacon-server/src/main/Constants.ts
@@ -1,12 +1,13 @@
-import path from 'path'
+import path from 'path';
+
 
 export const Constants = new (class {
     public readonly DATA_PATH =
         process.platform === 'win32'
-            ? path.join(process.env.APPDATA as string, '.mineadmin')
+            ? path.join(process.env.APPDATA as string, '.bacon')
             : process.platform === 'darwin'
-            ? path.join(process.env.HOME as string, 'Library', 'Application Support', 'MineAdmin')
-            : path.join(process.env.HOME as string, '.mineadmin')
+            ? path.join(process.env.HOME as string, 'Library', 'Application Support', 'BaCon')
+            : path.join(process.env.HOME as string, '.bacon')
 
     public readonly TEMP_DIR = path.join(this.DATA_PATH, 'temp')
 })()
